f18386c55cfaf2a04c3dd87baced798dad210276
Jay Merlan <jmerlan@amazon.com>
2021-11-04 15:56:39 -0700

    Added Manager class

---
+++ Samples.ContentManagement/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement
+{
+    public class Manager
+    {
+        public List<Asset> Assets { get; set; }
+        public List<Entities.Content> Families { get; set; }
+        public List<Entities.Library> Libraries { get; set; }
+
+        public static void CompareParameters(Asset asset, Entities.Content content, Entities.Parameter parameter)
+        {
+
+        }
+    }
+}
