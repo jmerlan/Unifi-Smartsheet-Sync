c16d7e9afe706d368bd438ec1b549c582c955f81
Jay Merlan <jmerlan@amazon.com>
2021-11-04 12:27:43 -0700

    Add Statt classes for Smartsheet integration

---
+++ Samples.ContentManagement/Asset.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace ContentManagement
+{
+    public class Asset
+    {
+        public string State { get; set; }
+        public string Description { get; set; }
+        public bool? Merchandisable { get; set; }
+        public string Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Vendor { get; set; }
+        public string ProcuredBy { get; set; }
+        public string ReceivedBy { get; set; }
+        public string InstalledBy { get; set; }
+        public string ConnectBy { get; set; }
+        public bool IsSynced { get; set; }
+        public string SyncComments { get; set; }
+        public string Department { get; set; }
+        public string[] SubDepartment { get; set; }
+        public double CapMultiplier { get; set; }
+
+        /// <summary>
+        /// Casts a Row from Smarthsheet to an Asset
+        /// </summary>
+        /// <param name="asset"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static Asset FromRow(Row row)
+        {
+            var asset = new Asset();
+
+            // Assign properties based on Smartsheet cell values
+            asset.State =   Connector.GetCellValue(row, 7276296175675268);
+            asset.Description = Connector.GetCellValue(row, 4571044452296580);
+            asset.Id = Connector.GetCellValue(row, 4590715224254340);
+            asset.Manufacturer = Connector.GetCellValue(row, 8280756402317188);
+            asset.Model = Connector.GetCellValue(row, 5133994405717892);
+            asset.Vendor = Connector.GetCellValue(row, 630394778347396);
+            asset.ProcuredBy = Connector.GetCellValue(row, 8162260536321924);
+            asset.ReceivedBy = Connector.GetCellValue(row, 1858222396073860);
+            asset.InstalledBy = Connector.GetCellValue(row, 6361822023444356);
+            asset.ConnectBy = Connector.GetCellValue(row, 4110022209759108);
+            asset.Department = Connector.GetCellValue(row, 6096515000231812);
+            asset.Merchandisable = IsMerchandisable(row);
+            
+            // Do work to get the multiple subdepartments as a list
+            var subDeptString = Connector.GetCellValue(row, 3844715186546564);
+            if (!string.IsNullOrEmpty(subDeptString))
+            { 
+                if (subDeptString.Contains(','))
+                {
+                    asset.SubDepartment = Array.ConvertAll(subDeptString.Split(','), p => p.Trim());
+                }
+                // If there is only one subdepartment, get the string as a single list item
+                else
+                {
+                    asset.SubDepartment = new string[] { subDeptString };
+                }
+            }
+
+            // Do work to get the capacity multiplier as a number
+            string capMultiplierString = Connector.GetCellValue(row, 512218688186244);
+            if (!string.IsNullOrEmpty(capMultiplierString))
+            {
+                try
+                {
+                    asset.CapMultiplier = Convert.ToInt32(capMultiplierString);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                asset.CapMultiplier = 1;
+            }
+
+            if (asset.CapMultiplier == 0)
+            {
+                asset.CapMultiplier = 1;
+            }
+
+            return asset;
+        }
+
+        /// <summary>
+        /// Checks if an Asset has customer interaction
+        /// </summary>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        public static bool? IsMerchandisable (Row row)
+        {
+            // Null until proven otherwise
+            bool? hasCx = null;
+
+            // Checkbox from Smartsheet returns a string. Cast this to a bool.
+            var smartsheetValue = Connector.GetCellValue(row, 3890439355950980);
+            if (smartsheetValue == "True")
+            {
+                hasCx = true;
+            }
+            if (smartsheetValue == "False")
+            {
+                hasCx = false;
+            }
+
+            return hasCx;
+        }
+    }
+}
---
+++ Samples.ContentManagement/Connector.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement
+{
+    public class Connector
+    {
+        const string BaseUrl = "https://api.smartsheet.com/2.0/sheets/";
+        public const long SheetIdMad = 2312799004190596;
+        public const long SheetIdStatt = 6172549672396676;
+        public Sheet MadSheet { get; set; }
+
+        // The API identifies columns by Id, but it's more convenient to refer to column names
+        public static Dictionary<string, long> ColumnMap;
+
+        public static Sheet InitializeSheet(long sheetId)
+        {
+            SmartsheetClient smartsheet = new SmartsheetBuilder()
+                .SetAccessToken(Secrets.SmartsheetKey)
+                .Build();
+
+            Sheet sheet = smartsheet.SheetResources.GetSheet(
+              sheetId,                    // sheetId
+              null,                       // IEnumerable<SheetLevelInclusion> includes
+              null,                       // IEnumerable<SheetLevelExclusion> excludes
+              null,                       // IEnumerable<long> rowIds
+              null,                       // IEnumerable<int> rowNumbers
+              null,                       // IEnumerable<long> columnIds
+              null,                       // Nullable<long> pageSize
+              null                        // Nullable<long> page
+            );
+
+            // Instantiate a new column map every time the Sheet is initialized
+            ColumnMap = new Dictionary<string, long>();
+
+            // Build column map for later reference
+            foreach (Column column in sheet.Columns)
+                ColumnMap.Add(column.Title, (long)column.Id);
+
+            return sheet;
+        }
+
+        public static List<Row> GetRows(Sheet sheet)
+        {
+            var rowsOut = new List<Row>();
+
+            foreach (Row r in sheet.Rows)
+            {
+                rowsOut.Add(r);
+            }
+
+            return rowsOut;
+        }
+
+        /// <summary>
+        /// Helper method to get the ID of a Column by its name.
+        /// </summary>
+        /// <param name="sheet">The Sheet to search through.</param>
+        /// <param name="name">The name of the Column.</param>
+        /// <returns>The ID of the Column.</returns>
+        public static long? GetColumnIdByName(Sheet sheet, string name)
+        {
+            long? colId = null;
+
+            foreach (var c in sheet.Columns)
+            {
+                if (c.Title.ToLower() == name.ToLower())
+                {
+                    colId = c.Id;
+                }
+            }
+
+            return colId;
+        }
+
+        /// <summary>
+        /// Get the value of a specific Cell on a Sheet.
+        /// </summary>
+        /// <param name="row">The Row object that the Cell belongs to.</param>
+        /// <param name="colId">The ID of the Column that the Cell belongs to.</param>
+        /// <returns>The value of a Cell formatted as a string.</returns>
+        public static string GetCellValue(Row row, long? colId)
+        {
+            // Return value
+            string value = String.Empty;
+
+            if (row != null)
+            {
+                // Loop through all Cells within Row to find the current Column
+                foreach (var cell in row.Cells)
+                {
+                    // If Column found by ID and the value is not null, return the value of the Cell
+                    if (cell.ColumnId == colId && cell.Value != null)
+                    {
+                        // Set the value of value to return
+                        value = cell.Value.ToString();
+                    }
+                }
+            }
+
+            return value;
+        }
+    }
+}
