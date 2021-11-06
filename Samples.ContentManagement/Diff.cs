0798de2b19defc53d888f90e8c5aa99b2938d899
Jay Merlan <jmerlan@amazon.com>
2021-11-05 23:53:01 -0700

    Added Diff class

---
+++ Samples.ContentManagement/Diff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContentManagement
+{
+    public class Diff
+    {
+        public string Key { get; set; }
+        public string AssetValue { get; set; }
+        public string FamilyValue { get; set; }
+
+        public static Diff CompareParameters(Asset asset, Entities.Content content)
+        {
+            if (asset.Manufacturer == content.Manufacturer)
+            {
+                // Return null because the Content is synced
+                return null;
+            }
+            else
+            {
+                // Pass the values to a Diff object and return
+                var diff = new Diff();
+
+                diff.Key = asset.Id;
+                diff.AssetValue = asset.Manufacturer;
+                diff.FamilyValue = content.Manufacturer;
+
+                return diff;
+            }
+        }
+    }
+}
--- Samples.ContentManagement/Manager.cs
+++ Samples.ContentManagement/Manager.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Smartsheet.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ContentManagement
 {
@@ -11,10 +13,6 @@ namespace ContentManagement
         public List<Asset> Assets { get; set; }
         public List<Entities.Content> Families { get; set; }
         public List<Entities.Library> Libraries { get; set; }
-
-        public static void CompareParameters(Asset asset, Entities.Content content, Entities.Parameter parameter)
-        {
-
-        }
+        public List<Diff> DiffList { get; set; }
     }
 }
